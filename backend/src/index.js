03257f6fdc40e107a28cdb99d8420a6a03defde2
Priyansh <shah.priyansh04@gmail.com>
2025-03-23 17:18:29 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://rubrics-one.vercel.app", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
